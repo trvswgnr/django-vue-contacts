9693b03d3002d72a7eb8fc59da19d0bf4cbe6627
trav <trvswgnr@gmail.com>
2018-11-18 22:38:19 -0500

    add static files

--- djangovue/djangovue/urls.py
+++ djangovue/djangovue/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from .routers import router
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
