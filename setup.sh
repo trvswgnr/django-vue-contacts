36c4c27f0892cb0b2f154078bdbcac23bcb0713c
trav <trvswgnr@gmail.com>
2018-11-23 09:09:55 -0500

    remove npm from setup

--- setup.sh
+++ setup.sh
@@ -3,9 +3,7 @@
 virtualenv venv -p $(which python3)
 source venv/bin/activate
 pip install Django
+pip install djangorestframework
 cd djangovue/
 python manage.py migrate
 python manage.py runserver
-cd contact/
-cd static/
-npm run dev
